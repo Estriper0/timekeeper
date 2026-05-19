d3f204434c4b120d4f970fa1b60f795eee341799
Sergei Kostoshko <m0kosnd0s@yandex.ru>
2026-05-19 11:44:12 +0300

    fix

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://159.194.217.94:8080'
+export const API_BASE_URL = 'http://159.194.217.94:8000'
 export const API_ENDPOINTS = {
   employees: `${API_BASE_URL}/api/employees`
 }
